464709cadd2a13f23fa6d9516320f7359ae56e12
Ray M <raymus254@gmail.com>
2020-10-21 23:17:40 +0300

    Fix for records get created
    Fixes issue where user record is not created when accessing /profile route, causing an exception to be thrown

--- src/Controller/FrontendUsersProfileController.php
+++ src/Controller/FrontendUsersProfileController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Bolt\UsersExtension\Controller;
 
+use Bolt\Controller\Backend\BackendZoneInterface;
 use Bolt\Configuration\Config;
 use Bolt\Configuration\Content\ContentType;
 use Bolt\Controller\Backend\ContentEditController;
@@ -15,11 +16,12 @@ use Bolt\Event\Listener\ContentFillListener;
 use Bolt\Repository\ContentRepository;
 use Bolt\UsersExtension\ExtensionConfigTrait;
 use Bolt\UsersExtension\Utils\ExtensionTemplateChooser;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class FrontendUsersProfileController extends AccessAwareController
+class FrontendUsersProfileController extends AccessAwareController implements BackendZoneInterface
 {
     use ExtensionConfigTrait;
 
@@ -32,27 +34,29 @@ class FrontendUsersProfileController extends AccessAwareController
     /** @var TwigAwareController */
     private $twigAwareController;
 
-    /** @var ContentEditController */
-    private $contentEditController;
-
     /** @var ExtensionTemplateChooser */
     private $templateChooser;
+
+    /** @var EntityManagerInterface */
+    private $em;
 
     public function __construct(
         Config $config,
         ContentRepository $contentRepository,
+        ContentEditController $contentEditController,
         ContentFillListener $contentFillListener,
         TwigAwareController $twigAwareController,
-        ContentEditController $contentEditController,
-        ExtensionTemplateChooser $templateChooser)
+        ExtensionTemplateChooser $templateChooser,
+        EntityManagerInterface $em)
     {
         parent::__construct($config);
 
         $this->contentRepository = $contentRepository;
+        $this->contentEditController = $contentEditController;
         $this->contentFillListener = $contentFillListener;
         $this->twigAwareController = $twigAwareController;
-        $this->contentEditController = $contentEditController;
         $this->templateChooser = $templateChooser;
+        $this->em = $em;
     }
 
     /**
@@ -67,7 +71,7 @@ class FrontendUsersProfileController extends AccessAwareController
         }
 
         $contentTypeSlug = $this->getExtension()->getExtConfig('contenttype', $user->getRoles()[0]);
-
+        
         /** @var ContentType $contentType */
         $contentType = $this->getBoltConfig()->getContentType($contentTypeSlug);
         $this->applyAllowForGroupsGuard($contentType);
@@ -91,6 +95,7 @@ class FrontendUsersProfileController extends AccessAwareController
 
         $content = $this->getUserRecord($contentType);
 
+        // buggy edit function
         if ($request->getMethod() === 'POST') {
             $this->contentEditController->save($content);
 
@@ -111,23 +116,62 @@ class FrontendUsersProfileController extends AccessAwareController
     {
         /** @var User $user */
         $user = $this->getUser();
+        
+        // Access user record, if available
+        $contentTypeSlug = $this->getExtension()->getExtConfig('contenttype', $user->getRoles()[0]);
+        $contentType = $this->getBoltConfig()->getContentType($contentTypeSlug);
+
         $content = $this->contentRepository->findBy([
             'author' => $user,
             'contentType' => $contentType->getSlug(),
         ]);
-
+        
+        // If user record unavailable, create it
         if (empty($content)) {
-            $content = new Content($contentType);
-            $content->setAuthor($user);
-            $content->setPublishedAt(new \DateTime());
-            $content->setStatus(Statuses::PUBLISHED);
-            $contentTypeName = strtolower($contentType->get('name', $contentType->get('slug')));
-            $content->setContentType($contentTypeName);
-            $this->contentFillListener->fillContent($content);
-        } elseif (is_iterable($content)) {
+            return $this->new($contentType);
+        } 
+        elseif (is_iterable($content)) {
             $content = end($content);
         }
-
+        
         return $content;
     }
+
+    // Define the user record content
+    private function new(ContentType $contentType): Content
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        $contentTypeSlug = $this->getExtension()->getExtConfig('contenttype', $user->getRoles()[0]);
+        $contentType = $this->getBoltConfig()->getContentType($contentTypeSlug);
+        
+        $content = new Content($contentType);
+        $content->setAuthor($user);
+        $content->setCreatedAt(new \DateTime());
+        $content->setPublishedAt(new \DateTime());
+        $content->setStatus(Statuses::PUBLISHED);
+        $contentTypeName = strtolower($contentType->get('name', $contentType->get('slug')));
+        $content->setContentType($contentTypeName);
+        $content->setFieldValue('displayName', $user->getDisplayName()); // Hidden field for record title
+        $content->setFieldValue('username', $user->getUsername()); // Hidden field with copy of username
+        
+        // Initialise ALL extra fields as defined in the contenttype with empty strings.
+        // This ensures they are displayed on the /profile/edit route without backend intervention
+        $content->setFieldValue('dob', '');
+
+        $content->setFieldValue('slug', $user->getUsername()); // Make slugs unique to users
+        $this->contentFillListener->fillContent($content);
+
+        // Persist in DB
+        $this->saveContent($content);
+        return $content;
+    }
+
+    // Saves user record to the database
+    private function saveContent(Content $content): void
+    {
+        $this->em->persist($content);
+        $this->em->flush();
+    }
+
 }
